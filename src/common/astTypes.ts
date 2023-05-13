8410cb7ab5ffd4f00ba3ab9fa916bdb72965e152
Dylan Sprague <dylan.richard.sprague@gmail.com>
2023-05-12 22:36:31 -0400

    Bring in fp-ts, newtype-ts; use newtype for Identifier type

--- src/common/astTypes.ts
+++ src/common/astTypes.ts
@@ -1,10 +1,14 @@
+import { Option } from "fp-ts/lib/Option";
+import { Newtype } from "newtype-ts";
 import { VariantOf, fields, variantModule } from "variant";
+
+export type Identifier = Newtype<{ readonly Identifier: unique symbol }, string>;
 
 const noFields = fields<Record<string, never>>();
 
 export const TopLevelDeclaration = variantModule({
   typeDeclaration: noFields,
-  functionDeclaration: fields<{ name: string }>(),
+  functionDeclaration: fields<{ name: Identifier }>(),
   // TODO - anything else?
 });
 export type TopLevelDeclaration = VariantOf<typeof TopLevelDeclaration>;
@@ -13,17 +17,17 @@ export const Expression = variantModule({
   intLiteral: fields<{ value: bigint }>(),
   fpLiteral: fields<{ value: number }>(),
   boolLiteral: fields<{ value: boolean }>(),
-  identifier: fields<{ name: string }>(), // TODO - rename to "variableRef" or similar?
+  identifier: fields<{ name: Identifier }>(), // TODO - rename to "variableRef" or similar?
   // TODO - operations, function calls, field accesses, match expressions, etc.
 });
 export type Expression = VariantOf<typeof Expression>;
 
 export const Statement = variantModule({
   statementExpression: fields<{ expression: Expression }>(),
-  variableDeclaration: fields<{ name: string; value: Expression }>(),
-  variableAssignment: fields<{ name: string; value: Expression }>(),
+  variableDeclaration: fields<{ name: Identifier; value: Expression }>(),
+  variableAssignment: fields<{ name: Identifier; value: Expression }>(),
   ifStatement: fields<{ condition: Expression; thenBlock: Block; elseBlock: Block }>(),
-  returnStatement: fields<{ returnedValue?: Expression }>(),
+  returnStatement: fields<{ returnedValue: Option<Expression> }>(),
   // TODO - loop statements, etc.
 });
 export type Statement = VariantOf<typeof Statement>;
