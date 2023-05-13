11ac06b08f14db1ab69f7313ed5eada6fad8cafa
Dylan Sprague <dylan.richard.sprague@gmail.com>
2023-05-12 23:05:22 -0400

    Add types for while/do-while/for loops

--- src/common/astTypes.ts
+++ src/common/astTypes.ts
@@ -8,7 +8,7 @@ const noFields = fields<Record<string, never>>();
 
 export const TopLevelDeclaration = variantModule({
   typeDeclaration: noFields,
-  functionDeclaration: fields<{ name: Identifier }>(),
+  functionDeclaration: fields<{ name: Identifier }>(), // TODO - other fields
   // TODO - anything else?
 });
 export type TopLevelDeclaration = VariantOf<typeof TopLevelDeclaration>;
@@ -17,8 +17,10 @@ export const Expression = variantModule({
   intLiteral: fields<{ value: bigint }>(),
   fpLiteral: fields<{ value: number }>(),
   boolLiteral: fields<{ value: boolean }>(),
+  // TODO - string literal, depending on how I end up designing that?
   identifier: fields<{ name: Identifier }>(), // TODO - rename to "variableRef" or similar?
-  // TODO - operations, function calls, field accesses, match expressions, etc.
+  // TODO - unary operations, binary operations, function calls, field accesses, match expressions
+  // NOTE - no lambdas (no higher-order functions) currently
 });
 export type Expression = VariantOf<typeof Expression>;
 
@@ -26,9 +28,12 @@ export const Statement = variantModule({
   statementExpression: fields<{ expression: Expression }>(),
   variableDeclaration: fields<{ name: Identifier; value: Expression }>(),
   variableAssignment: fields<{ name: Identifier; value: Expression }>(),
-  ifStatement: fields<{ condition: Expression; thenBlock: Block; elseBlock: Block }>(),
+  ifStatement: fields<{ condition: Expression; thenBlock: Block; elseBlock: Option<Block> }>(),
   returnStatement: fields<{ returnedValue: Option<Expression> }>(),
-  // TODO - loop statements, etc.
+  whileLoop: fields<{ condition: Expression; body: Block }>(),
+  doWhileLoop: fields<{ condition: Expression; body: Block }>(),
+  forLoop: fields<{ initializer: Expression; condition: Expression; increment: Expression; body: Block }>,
+  // TODO - struct field setting
 });
 export type Statement = VariantOf<typeof Statement>;
 
