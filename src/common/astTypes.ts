21935ac9bc31b9fc01685d4911788b5845b04dc6
Dylan Sprague <dylan.richard.sprague@gmail.com>
2023-05-23 16:36:12 -0400

    Add unary operations to Expression type
    Required reworking Expression variant to allow recursion

--- src/common/astTypes.ts
+++ src/common/astTypes.ts
@@ -1,6 +1,6 @@
 import { Option } from "fp-ts/lib/Option";
 import { Newtype, iso } from "newtype-ts";
-import { TypeNames, VariantOf, fields, variantModule } from "variant";
+import { TypeNames, Variant, VariantOf, fields, pass, typedVariant, variantList, variantModule } from "variant";
 
 // identifies names of variables and functions
 export type ValueIdentifier = Newtype<{ readonly Identifier: unique symbol }, string>;
@@ -11,22 +11,34 @@ export type TypeIdentifier = Newtype<{ readonly TypeIdentifier: unique symbol },
 export const typeIdentifierIso = iso<TypeIdentifier>();
 
 export const TopLevelDeclaration = variantModule({
-  typeDeclaration: {}, // TODO - fields
+  typeDeclaration: fields<{ name: TypeIdentifier }>(), // TODO - fields
   functionDeclaration: fields<{ name: ValueIdentifier }>(), // TODO - other fields
   // TODO - anything else?
 });
 export type TopLevelDeclaration = VariantOf<typeof TopLevelDeclaration>;
 
-export const Expression = variantModule({
-  intLiteral: fields<{ value: bigint }>(),
-  fpLiteral: fields<{ value: number }>(),
-  boolLiteral: fields<{ value: boolean }>(),
-  stringLiteral: fields<{ value: string }>(),
-  valueIdentifier: fields<{ name: ValueIdentifier }>(), // TODO - rename to "variableRef" or similar?
-  // TODO - unary operations, binary operations, function calls, field accesses, match expressions
-  // NOTE - no lambdas (no higher-order functions) currently
+export const UnaryOperation = variantList(["LogicalNot", "ArithmeticNegation"]);
+export type UnaryOperation = VariantOf<typeof UnaryOperation>;
+
+// need to use type-first approach to allow a recursive variant;
+// see https://paarthenon.github.io/variant/docs/use/advanced-creation#typedvariantt
+export type Expression =
+  | Variant<"IntLiteral", { value: bigint }>
+  | Variant<"FpLiteral", { value: number }>
+  | Variant<"BoolLiteral", { value: boolean }>
+  | Variant<"StringLiteral", { value: string }>
+  | Variant<"VariableRef", { name: ValueIdentifier }>
+  | Variant<"UnaryOperation", { operation: UnaryOperation; operand: Expression }>;
+export const Expression = typedVariant<Expression>({
+  IntLiteral: pass,
+  FpLiteral: pass,
+  BoolLiteral: pass,
+  StringLiteral: pass,
+  VariableRef: pass,
+  UnaryOperation: pass,
 });
-export type Expression = VariantOf<typeof Expression>;
+// TODO - binary operations, function calls, field accesses, match expressions
+// NOTE - no lambdas (no higher-order functions) currently
 
 // statements with no syntactic sugar, that can't be simplified further
 export const UnsugaredStatement = variantModule({
